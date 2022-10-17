97a39c423e71434f54302c6b8f6a6dcce0c3d0c9
ooze <grigorianna@gmail.com>
2022-10-17 15:48:43 +0400

    Added DayWeek

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Домашняя работа № 1 к семинару 17.10.2022
 
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int SecondOfThree(int number)
 {
     int des = number % 100;
@@ -20,4 +20,30 @@ else
     int result = SecondOfThree(num);
 
     Console.WriteLine($"{result} - это вторая цифра в числе {num}");
+}
+*/
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool DayWeek(int dayNum)
+{
+    if(dayNum == 6 || dayNum == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.WriteLine("Введите цифру соответствующую дню недели(от 1 до 7): ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 7)
+{
+    Console.WriteLine ("Эта цифра не соотвтствует дню недели!");
+}
+else
+{
+    bool result = DayWeek(num);
+    Console.Write("Этот день является выходным - ");
+    Console.WriteLine(result);
 }
