49e9f08fcfc48e7f9011566b936a06f272b45dfa
ooze <grigorianna@gmail.com>
2022-10-20 17:42:50 +0400

    Added Cube

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -26,7 +26,7 @@ else
 // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
-
+/*
 double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     double x = x2 - x1;
@@ -56,7 +56,25 @@ double z2 = Convert.ToInt32(Console.ReadLine());
 
 double res = Distance(x, y, z, x2, y2, z2);
 Console.WriteLine($"Расстояние между точками равно {Math.Round(res, 2)}");
-
-
+*/
 
 // Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int n)
+{
+    int i = 1;
+    Console.Write($"Кубы чисел от 1 до {n}: ");
+    while(i <= n)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+        i++;
+    }
+}
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n < 1)
+{
+    Console.WriteLine("Это некорректное число. Введите число больше единицы: ");
+    n = Convert.ToInt32(Console.ReadLine());    
+}
+Cube(n);
