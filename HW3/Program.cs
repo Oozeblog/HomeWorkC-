67be0139053b9a32700ee76fd6c0d0e96d02d56f
ooze <grigorianna@gmail.com>
2022-10-20 16:14:37 +0400

    Added Distance

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
+/*
 void Polindrome5(int num)
 {
     if( num % 10 == num / 10000 && (num/10) %10 == (num/1000)%10)
@@ -21,12 +21,42 @@ else
 {
     Polindrome5(num);
 }
-
-
-
+*/
 
 // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double x = x2 - x1;
+    double y = y2 - y1;
+    double z = z2 - z1;
+    double dist = Math.Sqrt(x*x + y*y + z*z);
+    return dist;
+}
+
+Console.WriteLine("Введите координаты первой точки по оси х: ");
+double x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки по оси y: ");
+double y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки по оси z: ");
+double z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки по оси х: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки по оси y: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки по оси z: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double res = Distance(x, y, z, x2, y2, z2);
+Console.WriteLine($"Расстояние между точками равно {Math.Round(res, 2)}");
+
+
 
 // Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
