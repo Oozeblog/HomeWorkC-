bd0aac62e273fd8196b827e31979dcee9dca1b90
ooze <grigorianna@gmail.com>
2022-10-21 21:50:11 +0400

    DayWeek new version

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -50,6 +50,7 @@ else
 */
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Вариант 1
 /*
 bool DayWeek(int dayNum)
 {
@@ -75,3 +76,39 @@ else
     Console.WriteLine(result);
 }
 */
+
+// Вариант 2. (с проверкой, не ввели ли название дня недели)
+
+bool DayWeek(int dayNum)
+{
+    if(dayNum == 6 || dayNum == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("Введите цифру соответствующую дню недели(от 1 до 7): ");
+var tostring = Console.ReadLine();
+
+    if (int.TryParse(tostring, out var num))
+    {
+        if(num > 7)
+        {
+            Console.WriteLine ("Эта цифра не соотвтствует дню недели!");
+        }
+        else
+        {
+            bool result = DayWeek(num);
+            Console.Write("Этот день является выходным - ");
+            Console.WriteLine(result);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод");
+    }
+
