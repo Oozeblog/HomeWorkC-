a97a0edc88735e2f40d43cd83071654bcef97793
ooze <grigorianna@gmail.com>
2022-11-10 20:40:25 +0400

    TrinityEvenNumber added

---
+++ HW5/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int[] CreateTrinityArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int HowManyEvenNumbers(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            even++;
+        }
+    }
+    return even;
+}
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateTrinityArray(n);
+ShowArray(myArray);
+int evenNumber = HowManyEvenNumbers(myArray);
+Console.WriteLine($"В данном массиве {evenNumber} чётных чисел");
+*/
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
