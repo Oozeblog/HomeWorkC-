a9a8e098aedf108e89f6cd07bdd8a2d0c51b3a95
ooze <grigorianna@gmail.com>
2022-11-17 18:17:54 +0400

    Added FindElement

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Домашняя работа к семинару от 17.11.2022
 // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+/*
 double [,] CreateRandom2dDoubleArray(int rows, int columns, int minValue, int maxValue)
 {
     double[,] array = new double [rows, columns];
@@ -36,14 +36,59 @@ int max = Convert.ToInt32(Console.ReadLine());
 double[,] myArray = CreateRandom2dDoubleArray(m, n, min, max);
 Console.WriteLine("Массив вещественных чисел: ");
 Show2dDoubleArray(myArray);
-
+*/
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+/*
+int [,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 
-
+void FindElement(int[,] array, int rows, int columns)
+{
+    if( rows > array.GetLength(0) || columns > array.GetLength(1)) Console.WriteLine("Элемента с такими параметрами не существует!");
+    else Console.WriteLine($"Искомый элемент: {array[rows,columns]}");
+}
+Console.Write("Введите колличество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Console.WriteLine("Исходный массив: ");
+Show2dArray(myArray);
+Console.Write("Введите позицию элемента в строке: ");
+int r = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента в столбце: ");
+int c = Convert.ToInt32(Console.ReadLine());
+FindElement(myArray, r, c);
+*/
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-/*
+
 int [,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] array = new int [rows, columns];
@@ -77,19 +122,16 @@ void ShowArray(double[] array)
 }
 double[] AverageColumns(int [,] array)
 {
-    double avg = 0;
-    
     double [] avgArray = new double[array.GetLength(1)];
 
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        int count = 0;
-        for (int i = 0; i < array.GetLength(1); i++)
+        double avg = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            avg = avg + array[i,j];
-            count ++;
+            avg += array[i, j];
         }
-        avgArray[j] = avg / count;
+        avgArray[j] = avg / array.GetLength(0);
     }
     return avgArray;
 }
@@ -109,4 +151,4 @@ Show2dArray(myArray);
 double [] arrayRes = AverageColumns(myArray);
 Console.WriteLine("Cреднее арифметическое элементов в каждом столбце: ");
 ShowArray(arrayRes);
-*/
+
