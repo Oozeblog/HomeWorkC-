d30f3d9592fd814e33289e869b8eb859e0ea8b38
ooze <grigorianna@gmail.com>
2022-11-17 15:08:36 +0400

    Added DoubleArray

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -1,13 +1,49 @@
 ﻿// Домашняя работа к семинару от 17.11.2022
 // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
+double [,] CreateRandom2dDoubleArray(int rows, int columns, int minValue, int maxValue)
+{
+    double[,] array = new double [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+        }
+    }
+    return array;
+}
+void Show2dDoubleArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i,j], 2) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите колличество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dDoubleArray(m, n, min, max);
+Console.WriteLine("Массив вещественных чисел: ");
+Show2dDoubleArray(myArray);
+
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
+/*
 int [,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] array = new int [rows, columns];
@@ -73,4 +109,4 @@ Show2dArray(myArray);
 double [] arrayRes = AverageColumns(myArray);
 Console.WriteLine("Cреднее арифметическое элементов в каждом столбце: ");
 ShowArray(arrayRes);
-
+*/
