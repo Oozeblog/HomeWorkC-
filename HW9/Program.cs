98d34d9a4119987b370f0d82648635ce0231c5c2
ooze <grigorianna@gmail.com>
2022-11-24 13:50:34 +0400

    Added ShowNatural

---
+++ HW9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+/*
+void ShowNatural(int n)
+{
+    Console.Write(n + " ");
+    if(n > 1) ShowNatural(n - 1);
+}
+Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowNatural(n);
+*/
+
